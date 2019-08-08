42c323ab86e3496809e128faeec7d3f9f5ed14f0
Jonathan Meier <jonathanwmeier@gmail.com>
2019-08-07 22:49:11 -0600

    Tie run_secret_injector into cli

--- src/secret_injector/cli.py
+++ src/secret_injector/cli.py
@@ -1,7 +1,10 @@
 import click
 
+from secret_injector.runner import run_secret_injector
+
 
 @click.command()
 def secret_injector_cli():
     click.echo('Hello World!')
+    run_secret_injector()
 
